cc58f999b3f50e1bfd591ae18923501aa8ab2e5f
LeonCookie <LeonGdynia1105@gmail.com>
2025-04-16 23:30:36 +0200

    dodatnie plikow gpx to plikow zrodlowych,
    dodanie komentarza dla pracy zdalnej w parze

--- ProjektGPX/ProjektGPX.cpp
+++ ProjektGPX/ProjektGPX.cpp
@@ -3,6 +3,15 @@
 int main()
 {
 	//z jakegos powodu plik nie wczutuje sie gdy uzywamy \ lub \\(blad cerr), ale juz tak gdy/ tak jak ponizej
-	PlikGPX plik("C:/Users/leonr/OneDrive/Pulpit/Gdynia.txt");
-	plik.wczytaj();
+	//trzeba zmienic plik .gpx na .txt
+
+	//L.R
+	PlikGPX GdyniaPlik("C:/Users/leonr/OneDrive/Pulpit/Gdynia.txt");
+	PlikGPX CzernicaPlik("C:/Users/leonr/OneDrive/Pulpit/Czernica.txt");
+	//O.R
+	/*
+	PlikGPX GdyniaPlik("sciezka rybaczyk");
+	PlikGPX CzernicaPlik("sciezka rybaczyk");
+	*/
+	GdyniaPlik.wczytaj();
 }
